ed4f298f59b22a2a1b733cbdd9b9e08fa85d0955
bmhungqb <21522110@gm.uit.edu.vn>
2023-01-08 20:13:25 +0700

    hz

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,14 @@
 import express from "express";
 import homeController from "../controllers/HomeController";
-import ChatbotControllers from "../controllers/ChatbotControllers";
+
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
-    router.post("/webhook", ChatbotControllers.postWebhook);
-    router.get("/webhook", ChatbotControllers.getWebhook);
-    return app.use("/", router)
+
+    router.post('/webhook', homeController.postWebhook);
+    router.get('/webhook', homeController.getWebhook)
+    return app.use('/', router);
 }
 
 module.exports = initWebRoutes;
